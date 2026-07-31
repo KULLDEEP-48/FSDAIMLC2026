2885f1ce55b1a1b4ec36366382ea34c0ebfa920d
KULLDEEP_48 <singhkuldeep790688@gmail.com>
2026-07-31 10:29:10 +0530

    #

--- index.js
+++ index.js
@@ -8,24 +8,24 @@
 // sumWithMsg(sum,"Hey calculation with js");
 
 
-function login(msg,error){
-    if(error){
-        console.log("Error: "+error);
-    }
-    else{
-        console.log("Success: "+msg);
-    }
-}
+// function login(msg,error){
+//     if(error){
+//         console.log("Error: "+error);
+//     }
+//     else{
+//         console.log("Success: "+msg);
+//     }
+// }
 
-function loginVerification(username,password,clbk){
-    if(username==="0325" && password==="0325"){
-        clbk("Login successful",null);
-    }
-    else{
-        clbk(null,"Invalid username or password");
-    }
-}
-loginVerification("0325","0325",login)
+// function loginVerification(username,password,clbk){
+//     if(username==="0325" && password==="0325"){
+//         clbk("Login successful",null);
+//     }
+//     else{
+//         clbk(null,"Invalid username or password");
+//     }
+// }
+// loginVerification("0325","0325",login)
 // Callback function
 
 // function checkNumber(num, clbk) {
@@ -39,3 +39,56 @@ loginVerification("0325","0325",login)
 //     }
 // }
 // checkNumber(7, oddEven);
+
+
+// console.log("first")
+// setTimeout(()=>{console.log("second")},1000)
+// for(i=0;i<10000;i++){
+//     console.log("second")
+// }
+// console.log("third")
+
+// setTimeout(()=>{ console.log("first")
+//     setTimeout(()=>{ console.log("second")
+//         setTimeout(()=>{ console.log("third")
+//             setTimeout(()=>{ console.log("fourth")
+//                 setTimeout(()=>{ console.log("fifth")
+//                 },100)
+//             },100)
+//         },100)
+//     },100)
+// },100)
+
+// PROMISES.
+
+const myPromise=new Promise((resolve,reject)=>{
+    username="kuldeep0111"
+    password="1531156"
+    if(username=="kuldeep0111" & password=="1531156"){
+        resolve("Success")
+    }
+    else{
+        reject("Failure")
+    }
+})
+
+// myPromise.then((msg)=>{
+//     console.log(msg)
+// }).catch((msg)=>{
+//     console.log(msg)
+// }).finally(()=>{
+//     console.log("All the resources have been closed and memory released")
+// })
+
+async function handlelogin(){
+    try{
+        await myPromise
+    }
+    catch(e){
+        console.log(e)
+    }
+    finally{
+        console.log("All the resources have been closed and memory released")
+    }
+}
+handlelogin();
