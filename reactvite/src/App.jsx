736558418d661c3f5e8c7d510fffa7bdb71d498d
KULLDEEP_48 <singhkuldeep790688@gmail.com>
2026-09-10 10:29:04 +0530

    react

---
+++ reactvite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import Icard from './component/ICard'
+
+function App() {
+  
+  return (
+    <div style={{ border: '7px solid white',height:'450px'}}>
+    <Icard></Icard>
+    </div>
+  )
+}
+
+export default App
---
+++ reactvite/src/Component/ICard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Icard() {
+
+  return (
+    <div  style={{ border: '10px solid red',height:'350px',width:'250px',marginLeft:'200px' }}>
+    <h2 style={{backgroundColor:'aqua',color:'black'}}>ABES Enginnering College</h2>
+    <h3 style={{ color: 'red' }}>Name : Kuldeep Singh </h3>
+    <h3 style={{ color: 'red' }}>Roll : 2400321530111 </h3>
+    <h3 style={{ color: 'red' }}>Branch : CSE-AIML</h3>
+    <h3 style={{ color: 'red' }}>Section : C </h3>
+    </div>
+    
+  )
+}
+
+export default Icard
